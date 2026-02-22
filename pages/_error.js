ecc6c5fe7beb4574923cd0b31ac26b1e54efe516
shin-asset-os <yonahararara@gmail.com>
2026-02-23 08:40:38 +0900

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -1,7 +1,4 @@
-export default function ErrorPage({ statusCode }) {
-  return <div>发生错误，状态码：{statusCode || 404}</div>
+const ErrorPage = () => {
+  return null
 }
-ErrorPage.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
-}
+export default ErrorPage
