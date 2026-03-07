6fad32720f8720c2b547eb836637ed1ad5f958ad
shin-asset-os <yonahararara@gmail.com>
2026-03-07 21:54:34 +0900

    Update blog.config.js
    10行目の最後にカンマを挿入

--- blog.config.js
+++ blog.config.js
@@ -7,7 +7,7 @@ const BLOG = {
   // Important page_id
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    '02ab3b8678004aa69e9e415905ef32a5'
+    '02ab3b8678004aa69e9e415905ef32a5',
 
   THEME: process.env.NEXT_PUBLIC_THEME || 'simple',
 
